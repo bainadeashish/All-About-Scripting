121ad38f2e68f44a5b8263d09f6c452e0e449a8c
Ashish Bainade <bainadeashish@gmail.com>
2016-08-05 20:44:46 +0530

    Extract text from any file

--- Scripting/textextracter.py
+++ Scripting/textextracter.py
@@ -1,16 +1,16 @@
 from tika import parser
 import pdb
 from nltk.tokenize import word_tokenize,sent_tokenize
-keys=set(['Phone'])
+keys=set(['Phone','email','skill'])
 File_Name=r'C:\Users\Ashish.Bainade@infovisionlabs.com\Desktop\vikas patil.doc'
 text = parser.from_file(File_Name)
 content = text['content']
 #words=word_tokenize(content)
-counter=0
-sentences=sent_tokenize(content)
+#sentences=sent_tokenize(content)
+sentences=set(content.splitlines())
 for each_sentence in sentences:
-	counter+=1
 	for key in keys:
+		
 		if key.lower() in each_sentence.strip('\n').lower():
-			print key ,counter , each_sentence.strip('\n')
+			print key , each_sentence.strip('\n')
 
